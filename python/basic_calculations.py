528ed53f9743250eacd8c1fb0a6f4675ac7dda0d
Ruan Vitorino Souza <vitorinoruan3@gmail.com>
2026-01-19 20:53:36 -0300

    Add basic electrical power calculation
    Add basic electrical power calculation in Python

---
+++ python/basic_calculations.py
@@ -0,0 +1,18 @@
+"""
+Basic engineering calculations.
+
+Author: Ruan Vitorino Souza
+Description:
+Simple electrical calculations used as part of engineering studies.
+"""
+
+# Electrical parameters
+voltage = 220  # Volts
+current = 5    # Amperes
+
+# Power calculation
+power = voltage * current
+
+print(f"Voltage: {voltage} V")
+print(f"Current: {current} A")
+print(f"Electrical Power: {power} W")
